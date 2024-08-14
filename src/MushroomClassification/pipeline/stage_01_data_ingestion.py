e21f6a2a0e818909a2cf9a17b8acd1c792ec7bf9
aman-srma <aman.srma.a7@gmail.com>
2024-08-14 17:43:15 +0530

    main py file updated

---
+++ src/MushroomClassification/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,30 @@
+from MushroomClassification.entity.config_entity    import DataIngestionConfig
+from MushroomClassification.components.data_ingestion import DataIngestion
+from MushroomClassification import logger
+from MushroomClassification.config.configuration import ConfigurationManager
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+    
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/MushroomClassification/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,27 @@
+from MushroomClassification.components.model_evaluation import ModelEvaluation
+from MushroomClassification.config.configuration import ConfigurationManager
+from MushroomClassification import logger
+
+STAGE_NAME = "Model Evaluation Stage"
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.log_into_mlflow()
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+        obj = ModelEvaluationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
